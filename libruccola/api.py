cecc043df3196f9996cb96cfebf929753398ad52
Kristoffer Grönlund <krig@koru.se>
2019-04-03 05:58:51 +0200

    Implement more of the rocket.chat API
    Add history, spotlight etc.

--- libruccola/api.py
+++ libruccola/api.py
@@ -38,6 +38,30 @@ class Channel(object):
         }
         return self._session.post("/api/v1/chat.postMessage", payload=payload)
 
+    def history(self, latest=None, oldest=None, offset=0, count=20, unreads=False):
+        """
+        Retrieves historical messages from channel.
+        """
+        payload = { "roomId": self.id }
+        if latest is not None:
+            payload["latest"] = latest
+        if oldest is not None:
+            payload["oldest"] = oldest
+        if offset > 0:
+            payload["offset"] = offset
+        if count != 20:
+            payload["count"] = count
+        if unreads:
+            payload["unreads"] = True
+        return self._session.post("/api/v1/channels.history", payload=payload)["messages"]
+
+    def online(self):
+        """
+        Lists online users in channel.
+        """
+        payload = {"_id": self.id}
+        return self._session.get("/api/v1/channels.online", payload=payload)["online"]
+
 
 class Session(object):
     def __init__(self, config):
@@ -103,4 +127,89 @@ class Session(object):
         response = self.get("/api/v1/channels.list.joined")
         return [Channel(self, channel) for channel in response["channels"]]
 
+    def spotlight(self, query):
+        """
+        Searches for users or rooms that are visible to the user.
+        Only returns rooms that the user hasn't joined yet.
+
+        query: Term to search for.
+
+        Returns the result of the search as a dict of two lists:
+        "users" and "rooms".
+        """
+        return self.get("/api/v1/spotlight", payload={"query": query})
+
+    def me(self):
+        """
+        Returns information about the user.
+
+        Example result:
+
+        {
+          "_id": "aobEdbYhXfu5hkeqG",
+          "name": "Example User",
+          "emails": [
+            {
+              "address": "example@example.com",
+              "verified": true
+            }
+          ],
+          "status": "offline",
+          "statusConnection": "offline",
+          "username": "example",
+          "utcOffset": 0,
+          "active": true,
+          "roles": [
+            "user",
+            "admin"
+          ],
+          "settings": {
+            "preferences": {
+              "enableAutoAway": false,
+              "idleTimeoutLimit": 300,
+              "desktopNotificationDuration": 0,
+              "audioNotifications": "mentions",
+              "desktopNotifications": "mentions",
+              "mobileNotifications": "mentions",
+              "unreadAlert": true,
+              "useEmojis": true,
+              "convertAsciiEmoji": true,
+              "autoImageLoad": true,
+              "saveMobileBandwidth": true,
+              "collapseMediaByDefault": false,
+              "hideUsernames": false,
+              "hideRoles": false,
+              "hideFlexTab": false,
+              "hideAvatars": false,
+              "roomsListExhibitionMode": "category",
+              "sidebarViewMode": "medium",
+              "sidebarHideAvatar": false,
+              "sidebarShowUnread": false,
+              "sidebarShowFavorites": true,
+              "sendOnEnter": "normal",
+              "messageViewMode": 0,
+              "emailNotificationMode": "all",
+              "roomCounterSidebar": false,
+              "newRoomNotification": "door",
+              "newMessageNotification": "chime",
+              "muteFocusedConversations": true,
+              "notificationsSoundVolume": 100
+            }
+          },
+          "customFields": {
+            "twitter": "@userstwi"
+          },
+          "success": true
+        }
+        """
+        return self.get("/api/v1/me")
+
+
+class Realtime(object):
+    """
+    WebSocket API for rocket.chat.
+    """
+    def __init__(self, config):
+        self._cfg = config
+        self._url = "wss://{server}/websocket".format(server=config.server)
 
